a01e8e9e45eb0d1e9a808b36381431c709878a50
Kao Pei-Wei <tif135@gmail.com>
2019-12-09 22:52:42 +0800

    addRotateCamera

---
+++ WorkOutApp/CameraController.swift
@@ -0,0 +1,100 @@
+//
+//  CameraController.swift
+//  WorkOutApp
+//
+//  Created by Kao Pei-Wei on 2019/12/6.
+//  Copyright © 2019 I CHIEN LAI. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
+    var backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if #available(iOS 10.2, *) {
+            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            do {
+                
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                captureSession = AVCaptureSession()
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.frame = view.layer.bounds
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            }
+            catch {
+                print("Error: Cannot load camera")
+            }
+        }
+    }
+    
+
+    @IBAction func imageCapture(_ sender: Any) {
+    }
+    
+    func switchToFrontCamera() {
+        if frontCamera?.isConnected == true {
+            captureSession?.stopRunning()
+            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                captureSession = AVCaptureSession()
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.frame = view.layer.bounds
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            }
+            catch {
+                print("Error")
+            }
+        }
+    }
+    
+    func switchToBackCamera() {
+        if backCamera?.isConnected == true {
+            captureSession?.stopRunning()
+            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                captureSession = AVCaptureSession()
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.frame = view.layer.bounds
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            }
+            catch {
+                print("Error")
+            }
+        }
+    }
+    
+    @IBAction func rotateCamera(_ sender: Any) {
+        guard let currentCameraInput: AVCaptureInput = captureSession?.inputs.first else {
+            return
+        }
+        
+        if let input = currentCameraInput as? AVCaptureDeviceInput {
+            if input.device.position == .back {
+                switchToFrontCamera()
+            }
+            if input.device.position == .front {
+                switchToBackCamera()
+            }
+        }
+    }
+
+
+}
